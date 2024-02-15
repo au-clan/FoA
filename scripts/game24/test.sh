2016f3291d75b07649d3d674fbb90192db88385b
Potamitisn <nearchosp@outlook.com>
2024-02-15 20:53:07 +0100

    Set up experiment

--- scripts/game24/test.sh
+++ scripts/game24/test.sh
@@ -1,8 +1,35 @@
 python run.py\
     --difficulty 9\
-    --n_samples 5\
+    --n_samples 50\
     --n_evaluations 3\
-    --n_agents 3\
+    --n_agents 5\
     --back_coef 0.8\
     --max_steps 10\
+    --model_name gpt-3.5-turbo-0125
+
+python run.py\
+    --difficulty 9\
+    --n_samples 50\
+    --n_evaluations 3\
+    --n_agents 10\
+    --back_coef 0.8\
+    --max_steps 10\
+    --model_name gpt-3.5-turbo-0125
+
+python run.py\
+    --difficulty 9\
+    --n_samples 50\
+    --n_evaluations 3\
+    --n_agents 15\
+    --back_coef 0.8\
+    --max_steps 10\
+    --model_name gpt-3.5-turbo-0125
+
+python run.py\
+    --difficulty 9\
+    --n_samples 50\
+    --n_evaluations 3\
+    --n_agents 10\
+    --back_coef 0.8\
+    --max_steps 20\
     --model_name gpt-3.5-turbo-0125
