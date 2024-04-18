4112d47db77356ae0adb7e6dfed7a77df6643995
Potamitisn <nearchosp@outlook.com>
2024-04-18 17:46:31 +0200

    Pushing results with gpt4 data

--- scripts/mixed.sh
+++ scripts/mixed.sh
@@ -6,5 +6,5 @@ seed__values=(0)
 
 for seed in "${seed__values[@]}"; do
     python async_implementation/experiments/gameof24.py --k 1 --max_steps 5 --n_agents 10 --backtrack 0.1 --set test --seed $seed --send_email
-    python async_implementation/experiments/crosswords.py --k 3 --max_steps 6 --n_agents 4 --backtrack 0.5 --set test --seed $seed --send_email
+    #python async_implementation/experiments/crosswords.py --k 3 --max_steps 6 --n_agents 4 --backtrack 0.5 --set test --seed $seed --send_email
 done
