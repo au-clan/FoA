a4369ce795f8c4255fba44718808014da65d93b6
Potamitisn <nearchosp@outlook.com>
2024-06-01 18:39:56 +0200

    Adding ws scripts

---
+++ scripts/ws/run.sh
@@ -0,0 +1,10 @@
+python async_implementation/experiments/ws.py\
+    --set mini\
+    --num_agents 5\
+    --backtrack 0.5\
+    --num_steps 15\
+    --resampling linear\
+    --k 3\
+    --repeats 1\
+    --prompting react\
+    --send_email
