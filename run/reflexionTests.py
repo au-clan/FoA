596e5816f07692885989a385a2a4b4f60c97189c
au681566 <au681566@uni.au.dk>
2025-04-25 15:14:29 +0200

    more type testing

--- run/reflexionTests.py
+++ run/reflexionTests.py
@@ -149,11 +149,11 @@ async def test_reflexion():
     num_reflexions_list = [4]  # Number of iterations to test
     k = 2  # k for "k most recent"
     num_agents = 4  
-    reflexion_types = ["list", "k most recent", "summary_incremental", "summary_all_previous"] #"list", "k most recent", "summary_incremental", "list", 
+    reflexion_types = ["list", "k most recent",  "summary_incremental", "summary_all_previous"]  
     results = []
     verifier = RafaVerifier()
 
-    for states in all_puzzles_data[3:15]:
+    for states in all_puzzles_data[6:15]:
         for i in range(num_agents):
             states[i] = states[0]
         for num_reflexions in num_reflexions_list:
