76d71869cdee06f11d6fa49547f0de28ed866f88
CoachMac99 <jonasnkarlsen@live.dk>
2025-06-03 12:08:46 +0200

    done with k most recent benchmark

---
+++ test/benchmarkKMostRecent.sh
@@ -0,0 +1,1 @@
+python run/rafa.py --backend gpt-4.1-nano-2025-04-14 --method_reflexion_type k_most_recent --k 8 --split uniform-test --feedback True --reflect True > outputFiles/BenchmarkKMostRecent.txt
